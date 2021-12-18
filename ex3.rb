ab158cc58e92eeb45b6fe95a9c0ccf076aaffe9d
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2021-12-17 19:02:23 -0500

    Complete exercise 3 - numbers and Math

---
+++ ex3.rb
@@ -0,0 +1,29 @@
+# Numbers and Math
+
+# + plus 
+# - minus 
+# / slashes 
+# * asterisk
+# % percemt
+# < less-than
+# > greater-than 
+# <= less-than-equal 
+# >= greater-than-equal 
+
+
+
+puts "I will now count my chickens:"
+
+puts "Hens #{20 + 30 / 6}" # the use of #{} in the code is how you insert Ruby computations into text strings. You can put anuthing that is ruby 
+puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts "Is it true that 3 + 2 < 5 -7?"
+puts 3 + 2 < 5 - 7
+puts "What is 3 + 2  #{3 + 2}?"
+puts "What is 5 - 7? #{5 - 7}"
+puts "Oh, that's why it's false."
+puts "How about some more."
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{ 5 <= -2}"

cb33cc9ce82d4cff275bded24d2df11779f38adf
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2021-12-18 13:52:08 -0500

    Complete exercise 34 - accessing elements of arrays

---
+++ ex34.rb
@@ -0,0 +1,10 @@
+# Exercise 34: Accessing Elements of Arrays
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+
+puts "My animals #{bear}, #{tiger}, #{penguin}."
+
+
